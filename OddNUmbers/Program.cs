83094fe8ec4ffdaa1bb4e98a472e9e4bd63b3220
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2024-09-20 13:42:14 +0400

    add odd numbers

---
+++ OddNUmbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Odd numbers to {num}: ");
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 1)
+    {
+        Console.Write(i + " ");
+    }
+    else 
+    { 
+        continue; 
+    }
+}
+
