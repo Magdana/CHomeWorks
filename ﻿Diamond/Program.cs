f7378f52007f7d7041fbaf5440339e999ed20652
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2024-09-20 19:00:38 +0400

    diamond

---
+++ ﻿Diamond/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("enter the number: ");
+int num = int.Parse(Console.ReadLine());
+
+string str = "";
+
+for (int i = 1; i <= num; i++)
+{
+    for (int j = 1; j <= (num - i); j++)
+    {
+        str += " ";
+    }
+
+    for (int m = 0; m < (2 * i - 1); m++)
+    {
+        str += "*";
+    }
+
+    str += "\n";
+}
+for (int i = num - 1; i >= 1; i--)
+{
+    for (int j = 1; j <= (num - i); j++)
+    {
+        str += " ";
+    }
+
+    for (int m = 0; m < (2 * i - 1); m++)
+    {
+        str += "*";
+    }
+
+    str += "\n";
+}
+
+Console.WriteLine(str);
