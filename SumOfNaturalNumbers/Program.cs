39302b8ce88ec922b0f7bddf622a69d4f1ba489b
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2024-09-20 13:31:41 +0400

    add sum of natural numbers project

---
+++ SumOfNaturalNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("enter the number: ");
+int num=int.Parse(Console.ReadLine());
+int sum = 0;
+Console.WriteLine($"The first {num} natural number is: ");
+for (int i=1; i<=num; i++)
+{
+    Console.Write($"{ i} ");
+}
+for (int i=1; i <= num; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"\nThe sum of Natural Number upto {num} terms: {sum}");
