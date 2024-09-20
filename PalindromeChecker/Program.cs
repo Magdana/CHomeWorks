8288eba4829c6b98137cdf119d30de0b0e14fda7
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2024-09-20 16:19:42 +0400

    add palindrome checker

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("input: ");
+string str=Console.ReadLine();
+char[] arr = str.ToCharArray();
+char[] newArr=arr.Reverse().ToArray();
+bool result=arr.SequenceEqual(newArr);
+if (result)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
